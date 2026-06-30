841f238dfc6e28a45f26011a4b7859dfa7aa24fb
Adarsh Bhagwat Shinde <202401040006@mitaoe.ac.in>
2026-06-30 10:09:02 +0530

    Remove trailing slash from CORS origin URL

--- server/src/app.js
+++ server/src/app.js
@@ -30,7 +30,7 @@ app.use(async (req, res, next) => {
 
 // Enable CORS for frontend client
 app.use(cors({
-  origin: 'https://planr-lovat.vercel.app/',
+  origin: 'https://planr-lovat.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
