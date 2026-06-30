87f77fe58bea2542ef13144de0ab168108555200
Adarsh Shinde <202401040006@mitaoe.ac.in>
2026-06-30 06:57:49 +0530

    first commit

---
+++ fix-author.sh
@@ -0,0 +1,5 @@
+﻿#!/bin/bash
+export GIT_COMMITTER_NAME='Adarsh Shinde'
+export GIT_COMMITTER_EMAIL='202401040006@mitaoe.ac.in'
+export GIT_AUTHOR_NAME='Adarsh Shinde'
+export GIT_AUTHOR_EMAIL='202401040006@mitaoe.ac.in'
